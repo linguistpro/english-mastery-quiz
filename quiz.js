61c02462e2987f84d130b02bbe1b53d68ddad331
linguistpro <patelsaurabhchandra90@gmail.com>
2026-07-04 04:17:51 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,113 +1,156 @@
-let currentQuestion=0;
-let score=0;
+let currentQuestion = 0;
+let score = 0;
 let timeLeft = 30;
 let timer;
-let selected=-1;
+let selected = -1;
 
-const question=document.getElementById("question");
-const options=document.getElementById("options");
-const number=document.getElementById("questionNumber");
-const progress=document.getElementById("progress");
-
-loadQuestion();
-
-function loadQuestion(){
-startTimer();
-selected=-1;
-
-question.innerHTML=questions[currentQuestion].question;
-
-number.innerHTML=`Question ${currentQuestion+1} / ${questions.length}`;
-
-progress.style.width=((currentQuestion+1)/questions.length*100)+"%";
-
-options.innerHTML="";
-
-questions[currentQuestion].options.forEach((item,index)=>{
-
-const div=document.createElement("div");
-
-div.className="option";
-
-div.innerHTML=item;
-
-div.onclick=function(){
-
-document.querySelectorAll(".option").forEach(o=>o.style.background="#eef4ff");
-
-div.style.background="#2563eb";
-
-div.style.color="white";
-
-selected=index;
-
-}
-
-options.appendChild(div);
-
-});
-
-}
-
-function nextQuestion(){
-
-if(selected==-1){
-
-alert("Please select an answer.");
-
-return;
-
-}
-
-if(selected===questions[currentQuestion].answer){
-
-score++;
-
-}
-
-currentQuestion++;
-
-if(currentQuestion<questions.length){
+const question = document.getElementById("question");
+const options = document.getElementById("options");
+const number = document.getElementById("questionNumber");
+const progress = document.getElementById("progress");
 
 loadQuestion();
 
-}else{
+function loadQuestion() {
 
-localStorage.setItem("score",score);
+    clearInterval(timer);
 
-window.location.href="result.html";
+    selected = -1;
+
+    const exp = document.getElementById("explanation");
+    if (exp) {
+        exp.style.display = "none";
+        exp.innerHTML = "";
+    }
+
+    question.innerHTML = questions[currentQuestion].question;
+
+    number.innerHTML = `Question ${currentQuestion + 1} / ${questions.length}`;
+
+    progress.style.width =
+        ((currentQuestion + 1) / questions.length * 100) + "%";
+
+    options.innerHTML = "";
+
+    questions[currentQuestion].options.forEach((item, index) => {
+
+        const div = document.createElement("div");
+
+        div.className = "option";
+
+        div.innerHTML = item;
+
+        div.onclick = function () {
+
+            if (selected !== -1) return;
+
+            selected = index;
+
+            clearInterval(timer);
+
+            document.querySelectorAll(".option").forEach(opt => {
+                opt.style.pointerEvents = "none";
+            });
+
+            if (index === questions[currentQuestion].answer) {
+
+                div.style.background = "green";
+                div.style.color = "white";
+
+                score++;
+
+            } else {
+
+                div.style.background = "red";
+                div.style.color = "white";
+
+                const correct =
+                    document.querySelectorAll(".option")[questions[currentQuestion].answer];
+
+                correct.style.background = "green";
+                correct.style.color = "white";
+            }
+
+            if (exp) {
+                exp.style.display = "block";
+                exp.innerHTML =
+                    questions[currentQuestion].explanation || "";
+            }
+
+            setTimeout(() => {
+                nextQuestion();
+            }, 2000);
+
+        };
+
+        options.appendChild(div);
+
+    });
+
+    startTimer();
 
 }
 
-}
-function startTimer(){
+function nextQuestion() {
 
-clearInterval(timer);
+    clearInterval(timer);
 
-timeLeft=30;
+    currentQuestion++;
 
-document.getElementById("timer").innerHTML=timeLeft+" sec";
+    if (currentQuestion < questions.length) {
 
-timer=setInterval(()=>{
+        loadQuestion();
 
-timeLeft--;
+    } else {
 
-document.getElementById("timer").innerHTML=timeLeft+" sec";
+        localStorage.setItem("score", score);
 
-if(timeLeft<=10){
+        window.location.href = "result.html";
 
-document.getElementById("timer").style.color="red";
-
-}
-
-if(timeLeft<=0){
-
-clearInterval(timer);
-clearInterval(timer);
-nextQuestion();
+    }
 
 }
 
-},1000);
+function startTimer() {
+
+    timeLeft = 30;
+
+    const timerElement = document.getElementById("timer");
+
+    timerElement.style.color = "black";
+
+    timerElement.innerHTML = timeLeft + " sec";
+
+    timer = setInterval(() => {
+
+        timeLeft--;
+
+        timerElement.innerHTML = timeLeft + " sec";
+
+        if (timeLeft <= 10) {
+            timerElement.style.color = "red";
+        }
+
+        if (timeLeft <= 0) {
+
+            clearInterval(timer);
+
+            currentQuestion++;
+
+            if (currentQuestion < questions.length) {
+
+                loadQuestion();
+
+            } else {
+
+                localStorage.setItem("score", score);
+
+                window.location.href = "result.html";
+
+            }
+
+        }
+
+    }, 1000);
 
 }
