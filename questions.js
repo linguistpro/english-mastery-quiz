08ce9a6318b612380e60824b640bf8c3ae60b1fa
linguistpro <patelsaurabhchandra90@gmail.com>
2026-07-04 03:04:03 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,12 @@
+const questions = [
+{
+question: "Choose the correct spelling.",
+options: ["Recieve", "Receive", "Receeve", "Receve"],
+answer: 1
+},
+{
+question: "What is the synonym of Happy?",
+options: ["Sad", "Joyful", "Angry", "Weak"],
+answer: 1
+}
+];
