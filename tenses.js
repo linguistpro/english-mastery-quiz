455dcfcd184ecb545b325c163bada867e8dd2c34
linguistpro <patelsaurabhchandra90@gmail.com>
2026-07-04 17:42:43 +0530

    Update tenses.js

--- tenses.js
+++ tenses.js
@@ -1,1 +1,14 @@
-
+const questions = [
+{
+question:"I ___ cricket every Sunday.",
+options:["play","played","playing","plays"],
+answer:0,
+explanation:"Simple Present uses the base verb."
+},
+{
+question:"She ___ TV yesterday.",
+options:["watch","watched","watches","watching"],
+answer:1,
+explanation:"Yesterday indicates Past Simple."
+}
+];
