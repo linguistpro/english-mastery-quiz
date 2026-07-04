abec4a30dd098ebc4227b606aab2f4f7d739683f
linguistpro <patelsaurabhchandra90@gmail.com>
2026-07-04 17:41:31 +0530

    Update mixed.js

--- mixed.js
+++ mixed.js
@@ -1,1 +1,14 @@
-
+const questions = [
+{
+question:"Choose the correct article.",
+options:["a apple","an apple","the apple","apple"],
+answer:1,
+explanation:"Use 'an' before vowel sounds."
+},
+{
+question:"What is the synonym of Fast?",
+options:["Slow","Quick","Weak","Hard"],
+answer:1,
+explanation:"Quick means Fast."
+}
+];
