38808f7db42eff8697e06f2e3917cbbca11da84d
linguistpro <patelsaurabhchandra90@gmail.com>
2026-07-04 17:42:12 +0530

    Update spoken.js

--- spoken.js
+++ spoken.js
@@ -1,1 +1,14 @@
-
+const questions = [
+{
+question:"How do you greet someone in the morning?",
+options:["Good Morning","Good Night","Bye","Welcome"],
+answer:0,
+explanation:"Good Morning is the correct greeting."
+},
+{
+question:"Someone says 'Thank you'. What do you reply?",
+options:["Sorry","You're Welcome","Hello","Bye"],
+answer:1,
+explanation:"'You're Welcome' is the polite reply."
+}
+];
