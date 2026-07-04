4f80e7debb46a5f16d4d6f6b2d16fcd8fee69e58
linguistpro <patelsaurabhchandra90@gmail.com>
2026-07-04 17:43:26 +0530

    Update vocabulary.js

--- vocabulary.js
+++ vocabulary.js
@@ -1,1 +1,14 @@
-
+const questions = [
+{
+question:"What is the synonym of Happy?",
+options:["Sad","Joyful","Weak","Hard"],
+answer:1,
+explanation:"Joyful means Happy."
+},
+{
+question:"What is the antonym of Rich?",
+options:["Poor","Kind","Tall","Soft"],
+answer:0,
+explanation:"Poor is opposite of Rich."
+}
+];
